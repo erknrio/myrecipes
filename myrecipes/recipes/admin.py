22bb705cd8317077824aea07c8e82e22bfcff54e
erknrio <erknrio@gmail.com>
2017-03-20 21:32:19 +0000

    More recipes options
    Ingredientes, price, difficult, etc.

--- myrecipes/recipes/admin.py
+++ myrecipes/recipes/admin.py
@@ -1,5 +1,15 @@
-from django.contrib import admin
+from django.contrib.admin import admin
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils.text import slugify
 
 from .models import Recipe
+
+
+@receiver(pre_save, sender=Recipe)
+def genera_slug(sender, instance, using, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.title)
+
 
 admin.site.register(Recipe)
--- myrecipes/recipes/models.py
+++ myrecipes/recipes/models.py
@@ -4,8 +4,21 @@ from django.contrib.auth.models import User
 
 class Recipe(models.Model):
     title = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField()
     short_description = models.CharField(max_length=140)
-    # datetime_created = models.DateTimeField(default=timezone.now)
+    difficult = models.OneToOneField(Difficult, on_delete=models.CASCADE)
+    directions_time = models.PositiveSmallIntegerField()
+    price = models.PositiveSmallIntegerField()
+    person_number = models.PositiveSmallIntegerField()
+    calories = models.PositiveSmallIntegerField()
+    thematic = models.OneToOneField(Thematic, on_delete=models.CASCADE)
+    ingredient = models.ManyToManyField(Ingredient)
+    directions = models.TextField()
+    nutricional_value = models.PositiveSmallIntegerField()
+    conservation = models.CharField(max_length=140)
+    # FIXME Este campo podria ser un textfield
+    # o mejor un enlace a otra receta existente
+    reuse = models.ForeignKey('self', on_delete=models.CASCADE)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modificated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User)
@@ -21,3 +34,43 @@ class Recipe(models.Model):
 #     @staticmethod
 #     def foo():
 #         print("hi")
+
+
+class Difficult(models.Model):
+    description = models.CharField(max_length=50)
+
+
+class DishType(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    # Starter, main, dessert, etc.
+    description = models.CharField(max_length=50)
+
+
+class FoodType(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    # vegetarian, gluten free, etc.
+    description = models.CharField(max_length=50)
+
+
+class Thematic(models.Model):
+    # spanish, mexican, chinese, tc.
+    description = models.CharField(max_length=50)
+
+
+class PublicTarget(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    # children, birthday, professional events, etc.
+    description = models.CharField(max_length=50)
+
+
+class Ingredient(models.Model):
+    description = models.CharField(max_length=50)
+
+
+class IngredientAlternative(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    description = models.CharField(max_length=50)
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(User)
